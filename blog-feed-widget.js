c5c4f6e45e7898381d14c248c6fd1ceff576a10b
Joshua <jwells@vcfcorp.com>
2016-10-01 00:26:13 -0400

    Create blog-feed-widget External Script
    Parse blog feed and display tiles of results

---
+++ blog-feed-widget.js
@@ -0,0 +1,39 @@
+var siteDomain = document.domain; 
+var blogFeed; 
+var dataCategory = "bedroom"; 
+if(siteDomain.indexOf("valuecityfurniture.com") != -1) { 
+	var blogFeed = "http://www.valuecityfurniture.com/blog/space-planning-tips/bedroom/feed/";
+}
+else { 
+    var blogFeed = "http://www.americansignaturefurniture.com/blog/space-planning-tips/bedroom/feed/"; 
+}
+
+$.ajax({
+  url      : 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(blogFeed),
+  dataType : 'json',
+  success  : function (data) {
+
+    var entry = data.responseData.feed.entries;
+    
+    for (var i = 0; i < entry.length; i++) {
+     var postTitle = entry[i].title;
+     var postSnippet = entry[i].contentSnippet;
+     var postLead = jQuery.trim(postSnippet).substring(0, 100).split(" ").slice(0, -1).join(" ");
+     var postLink = entry[i].link;
+     var postContent = entry[i].content;
+      
+      var imgContent = entry[i].content;
+      var imgDiv = document.createElement('div');
+      imgDiv.innerHTML = postContent;
+      var postImage = imgDiv.getElementsByTagName('img')[0];
+      var postImgSrc = postImage ? postImage.getAttribute("src") : "";
+
+      $("#blog-feed").prepend('<div class="panel panel-default blog-panel" style="box-shadow:none;margin-top:0;margin-bottom:0;margin-left:0.375em;margin-right:0.75em;"><div class="panel-body blog-panel-body"><a href="'+postLink+'" target="_blank" class="thumbnail" style="padding-bottom:15px;"><img src="'+postImgSrc+'" class="img-responsive center-block"><h4>'+postTitle+'</h4><p style="padding:0px 10px;font-size:14px;">'+postLead+'...<span style="color:#cc0000;">read more</span></p></a></div></div>');
+
+    }
+  },
+  error : function (data){
+  	alert("error: "+data);
+  }
+});
+$('#hawkcustomhtml a').on('click', function(event){  var eventCategory = "click"; var ePosition = $(this).attr('data-position');  var eAction = this.text;  var eLabel = this.href+':pos='+ePosition;  if(!eAction){  var eAction = $(this).find('img').attr('alt');  } console.log('event: '+eventCategory+' | action: '+eAction+' | label: '+eLabel); ga('send', {  hitType: 'event', eventCategory: 'click', eventAction: eAction, eventLabel: eLabel  })});
