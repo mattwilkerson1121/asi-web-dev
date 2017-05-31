fe003fdbf4b1f9ba6ded6a908f09ffa7553d1d85
joshua <jwells@co01-mac-jwells.vcfcorp.com>
2017-05-31 13:31:37 -0400

    Update Shoptelligence Code
    Update Shoptelligence Code

---
+++ shoptelligence/index.php
@@ -0,0 +1,198 @@
+<link href="https://shopvcf.com/css/asi-mix-styles-main.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+	.btn-group {
+		padding: 5px !important;
+	}
+
+	#ens2, #ens3, #ens4{	
+		display: none ;
+		width: 100% !important;
+		margin: auto !important; 
+	}
+	.addSingleProduct {
+		color: #E4002B !important;
+		border-color: #E4002B !important;
+	}		
+	.default {
+		color: black;
+	}
+
+	.productGrid {
+		padding: 0px 0px 20px 0px !important;
+	}
+
+	.btn:focus, .btn:active {
+	   outline: none !important;
+	}	
+	#alert {
+		margin-bottom: 0px !important;
+		padding-top: 9px !important;
+		padding-bottom: 9px !important;	
+		margin-right: 4px !important;		
+	}
+	#pe-container-a5be8c40-4b0a-41f5-b56c-3b5b307961e0 { 
+	    display:none !important; 
+	}	
+</style>
+<div class="asi-section-content asi-section section" id="asi-mix-browse-style" style="padding: 10px 0px 0px 0px;">
+<div class="container asi-container">
+<div class="asi-content asi-content-heading">
+<h3 class="asi-display-3 asi-darker">MIX</h3>
+
+<div class="asi-subheading">
+<div class="asi-subheading-rule"><span class="asi-subheading-text text-center">verb (to mix), mixed or mixt, mix-ing</span></div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="mixboard-ui" style="margin: 10px;">
+	<div class="row" id="header">
+		<div class="col-md-12 btn-toolbar" id="controls" style="padding: 0px;">
+			<button class="btn btn-outline pull-right" id="next" style="color: black !important;border: 1px solid rgb(128, 128, 128); background-color: rgb(255, 255, 255);" type="button">
+					Next MIX&nbsp;<span class="glyphicon glyphicon-arrow-right" aria-hidden="true" style="color: black !important;"></span>
+			</button>
+			<a href="http://www.valuecityfurniture.com/shoppingcart" target="_blank">
+				<button class="btn btn-outline pull-right" style="color: black !important;border: 1px solid rgb(128, 128, 128); background-color: rgb(255, 255, 255);" type="button">
+					<img alt="" src="/Content/ValueCityMix/svg/Cart_Icon.svg" width="14px;" />&nbsp;Cart
+				</button> 
+			</a>
+		</div>
+	</div>
+<!-- Start the Featured Product Section-->
+
+<div class="row" id="ens1">
+<div class="col-md-3 col-sm-3" style="text-align: center;"><!-- Anchor Item Space-->
+<h4 id="featuredHeadline">Design a room around this item:</h4>
+<img alt="" class="img-responsive center-block featureImg" data-iid="" src="https://placeholdit.imgix.net/~text?txtsize=30&amp;bg=ffffff&amp;txtclr=000000&amp;w=320&amp;h=320" />
+<h4>HOW TO MIX</h4>
+
+<p>MAKE IT YOU by combining, blending, or MIXing your favorite pieces of furniture to create a style that’s unique to you.</p>
+</div>
+
+<div class="col-md-9 col-sm-9">
+<div class="row"><br />
+<!-- Row 1 of the ensemble grid -->
+<div class="col-md-3 col-sm-3 col-xs-6" id="key1">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key2">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key3">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key4">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key5">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key6">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key7">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key8">&nbsp;</div>
+
+<div class="clearfix">&nbsp;</div>
+</div>
+</div>
+</div>
+<!-- Start the Featured Product Section-->
+
+<div class="row" id="ens2">
+<div class="col-md-3 col-sm-4" style="text-align: center;"><!-- Anchor Item Space-->
+<h4 id="featuredHeadline">Design a room around this item:</h4>
+<img alt="" class="img-responsive center-block featureImg" data-iid="" src="https://placeholdit.imgix.net/~text?txtsize=30&amp;bg=ffffff&amp;txtclr=000000&amp;w=320&amp;h=320" />
+<h4>HOW TO MIX</h4>
+
+<p>MAKE IT YOU by combining, blending, or MIXing your favorite pieces of furniture to create a style that’s unique to you.</p>
+</div>
+
+<div class="col-md-9 col-sm-8">
+<div class="row"><br />
+<!-- Row 1 of the ensemble grid -->
+<div class="col-md-3 col-sm-3 col-xs-6" id="key1">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key2">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key3">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key4">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key5">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key6">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key7">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key8">&nbsp;</div>
+
+<div class="clearfix">&nbsp;</div>
+</div>
+</div>
+</div>
+<!-- Start the Featured Product Section-->
+
+<div class="row" id="ens3">
+<div class="col-md-3 col-sm-4" style="text-align: center;"><!-- Anchor Item Space-->
+<h4 id="featuredHeadline">Design a room around this item:</h4>
+<img alt="" class="img-responsive center-block featureImg" data-iid="" src="https://placeholdit.imgix.net/~text?txtsize=30&amp;bg=ffffff&amp;txtclr=000000&amp;w=320&amp;h=320" />
+<h4>HOW TO MIX</h4>
+
+<p>MAKE IT YOU by combining, blending, or MIXing your favorite pieces of furniture to create a style that’s unique to you.</p>
+</div>
+
+<div class="col-md-9 col-sm-8">
+<div class="row"><br />
+<!-- Row 1 of the ensemble grid -->
+<div class="col-md-3 col-sm-3 col-xs-6" id="key1">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key2">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key3">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key4">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key5">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key6">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key7">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key8">&nbsp;</div>
+
+<div class="clearfix">&nbsp;</div>
+</div>
+</div>
+</div>
+<!-- Start the Featured Product Section-->
+
+<div class="row" id="ens4">
+<div class="col-md-3 col-sm-4" style="text-align: center;"><!-- Anchor Item Space-->
+<h4 id="featuredHeadline">Design a room around this item:</h4>
+<img alt="" class="img-responsive center-block featureImg" data-iid="" src="https://placeholdit.imgix.net/~text?txtsize=30&amp;bg=ffffff&amp;txtclr=000000&amp;w=320&amp;h=320" />
+<h4>HOW TO MIX</h4>
+
+<p>MAKE IT YOU by combining, blending, or MIXing your favorite pieces of furniture to create a style that’s unique to you.</p>
+</div>
+
+<div class="col-md-9 col-sm-8">
+<div class="row"><br />
+<!-- Row 1 of the ensemble grid -->
+<div class="col-md-3 col-sm-3 col-xs-6" id="key1">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key2">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key3">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key4">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key5">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key6">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key7">&nbsp;</div>
+
+<div class="col-md-3 col-sm-3 col-xs-6" id="key8">&nbsp;</div>
+
+<div class="clearfix">&nbsp;</div>
+</div>
+</div>
+</div>
+</div>
---
+++ shoptelligence/shoptelligence-widget.js
@@ -0,0 +1,356 @@
+		var articleKeysArray;
+		var artKeyIndex;
+		var featureImg = $(".au-product-main-images").attr("src").replace("product-650x650","product-320x320");
+		var sku = $("span.product-id-label").text();
+		console.log(sku);
+
+		var obj;
+
+		var cKeyOrder;
+		var cKey;
+		var swatchImgURL;
+		var item_id;
+		var product_id;
+		var pdpURL;		
+
+		var url = "http://shopvcf.com/asiapi/shoptelligence_api_prod.php?items="+sku;
+
+		$.ajax({
+			method:"GET",
+			dataType: "json",
+			url: url,
+			success: success_handler,
+		});
+
+		function success_handler(data){
+		    var obj = data;
+			console.log(obj);
+
+			if (obj == "Unable to suggest any outfits based on the currently selected anchor item(s)") {
+			// do nothing; no results found
+				$("#mixboard-ui").hide();
+				$("#asi-mix-browse-style").hide();
+			}
+			else {
+				var ens; 
+				// replace the img src of the featured product defaults in the html
+				// featured product is always returned in the 0 position
+				// of the first ensemble of the response object
+				$(".featureImg").attr('src', featureImg);	
+				// add the data-iid attribute to the featureImg
+				$(".featureImg").attr('data-iid', sku);
+
+				for (ens = 0; ens < obj.length; ens++) {
+				    var ensemble  = data[ens].ensemble;
+					var articleKeysArray0 = data[0].articleKeys;
+					var articleKeysArray1 = data[1].articleKeys;
+					var articleKeysArray2 = data[2].articleKeys;
+					var articleKeysArray3 = data[3].articleKeys;
+					if (ens == 0) {
+						console.log("ensemble below: "+ens);
+						console.log(ensemble);					
+						console.log("articleKeysArray below: "+ens);
+						console.log(articleKeysArray0);		
+						var ensembleNum = 0;				
+					}	
+					else if (ens == 1) {
+						//console.log("ensemble below: "+ens);
+						//console.log(ensemble);					
+						//console.log("articleKeysArray below: "+ens);
+						//console.log(articleKeysArray1);	
+						var ensembleNum = 1;					
+					}
+					else if (ens == 2) {
+						//console.log("ensemble below: "+ens);
+						//console.log(ensemble);					
+						//console.log("articleKeysArray below: "+ens);
+						//console.log(articleKeysArray2);	
+						var ensembleNum = 2;					
+					}		
+					else if (ens == 3) {
+						//console.log("ensemble below: "+ens);
+						//console.log(ensemble);					
+						//console.log("articleKeysArray below: "+ens);
+						//console.log(articleKeysArray3);	
+						var ensembleNum = 3;					
+					}																		
+
+					
+					// since index[0] of ensemble 0 is the featured product
+					// skip to index[1]
+					for (i=0; i < ensemble.length; i++) {
+						var imgURL = ensemble[i].image_url;
+						//var imgIndex = i;
+						var cKeyOrder = ensemble[i].canvas_key_order;
+						var cKey = ensemble[i].canvas_key;//.charAt(0)/*.toUpperCase() + ensemble[i].canvas_key.substr(1);//.toLowerCase();
+						var swatchImgURL = ensemble[i].swatch_image_url;
+						var item_id = ensemble[i].item_id;
+						var product_id = ensemble[i].product_id;
+						var pdpURL = 'http://www.valuecityfurniture.com/product/item/'+product_id;
+
+						if (ens == 0) {
+							var artKeyIndex = jQuery.inArray(item_id, articleKeysArray0);
+							console.log("item_id: " + item_id + " artKeyIndex: " + artKeyIndex + " data-ensIndex: "+ i + " canvas_key: " + cKey);			
+						}	
+						else if (ens == 1) {
+							var artKeyIndex = jQuery.inArray(item_id, articleKeysArray1);
+							console.log("item_id: " + item_id + " artKeyIndex: " + artKeyIndex + " data-ensIndex: "+ i + " canvas_key: " + cKey);
+						}
+						else if (ens == 2) {
+							var artKeyIndex = jQuery.inArray(item_id, articleKeysArray2);
+							console.log("item_id: " + item_id + " artKeyIndex: " + artKeyIndex + " data-ensIndex: "+ i + " canvas_key: " + cKey);
+						}		
+						else if (ens == 3) {
+							var artKeyIndex = jQuery.inArray(item_id, articleKeysArray3);
+							console.log("item_id: " + item_id + " artKeyIndex: " + artKeyIndex + " data-ensIndex: "+ i + " canvas_key: " + cKey);
+						}
+
+					  	var boardNum = ens+1;
+					  	var selectorStr = "#ens"+boardNum+" div#key"+i;
+
+  				  	  	$(selectorStr).append("<div id='productResults' style='text-align:center;'><div class='btn-toolbar'><button data-iid='"+item_id+"' data-ens='"+ensembleNum+"' data-ensIndex='"+i+"' data-cKey='"+cKey+"'  data-artKeyIndex='"+artKeyIndex+"' type='button' class='btn pull-right itemReplace' style='border: 1px solid rgb(128, 128, 128); background-color: rgb(255, 255, 255);'><span class='glyphicon glyphicon-refresh default' aria-hidden='true' ></button>&nbsp;<button id='addCartSingleProduct' data-cart='"+product_id+"'' type='button' class='btn btn-outline pull-right' style='border: 1px solid rgb(128, 128, 128); background-color: rgb(255, 255, 255);'></span><span class='glyphicon glyphicon-shopping-cart default' aria-hidden='true'></span></button></div><a href='"+pdpURL+"' target='_blank'><img class='img-responsive center-block productGrid' src='"+imgURL+"?impolicy=product-320x320' alt='image"+i+"' /></a></div>");						
+					}
+				}
+				// Click handler for the Item Replace API Call
+				$("div[id^=key]").on('click','button.itemReplace',function(){
+					var ensindex = parseInt($(this).data('ensindex'))
+					//console.log("articleKeysArray below: ");
+					//console.log(articleKeysArray);
+					// get the button
+					var itemRepalceButton = $(this);
+
+					var removeItem = $(this).attr("data-iid");
+					var removeItemArtKey = $(this).attr('data-artKeyIndex');
+					console.log("removeItem: "+removeItem+ " removeItemArtKey: "+removeItemArtKey);
+	
+					// find the element to replace selector
+					var itemReplace = "#"+$('div[id^=ens]:visible').attr('id')+" div#"+$(this).parent().parent().attr("id")+" img";
+					console.log("itemReplace: "+itemReplace);
+					// swap with a temporary image
+					$(itemReplace).attr('src', 'https://placeholdit.imgix.net/~text?txtsize=30&bg=ffffff&txtclr=000000&txt=Next+Item+Coming+Up&w=320&h=320');
+					// selector for the link to wrap around new image
+					var itemReplaceLink = "#"+$('div[id^=ens]:visible').attr('id')+" div#"+$(this).parent().parent().attr("id")+" a";
+
+					if ($(this).attr('data-ens') == 0) {
+						articleKeysArray0.splice(removeItemArtKey, 1);	
+						console.log("articleKeysArray after removing specific index: ");
+						console.log(articleKeysArray0);		
+						// make the item replacement call
+						var url = "http://shopvcf.com/asiapi/shoptelligence_api_prod.php?items="+articleKeysArray0;
+						console.log(url);	
+
+						$.ajax({
+							method:"GET",
+							dataType: "json",
+							url: url,
+							success: function(data) {
+								console.log(data);
+
+								var replaceImgURL = data[1].ensemble[ensindex].image_url+"?impolicy=product-320x320";
+								//console.log(replaceImgURL);
+								var cKeyOrder = data[1].ensemble[ensindex].canvas_key_order;
+								var swatchImgURL = data[1].ensemble[ensindex].swatch_image_url;
+								var new_item_id = data[1].ensemble[ensindex].item_id;
+								//console.log(item_id);
+								var product_id = data[1].ensemble[ensindex].product_id;
+								//console.log(product_id);
+								var pdpURLreplace = 'http://www.valuecityfurniture.com/product/item/'+product_id;
+								console.log(pdpURLreplace);	
+								// replace the placeholder image, with replacement item
+								$(itemReplace).attr('src', replaceImgURL);	
+								// replace link
+								console.log(itemReplaceLink);
+								$(itemRepalceButton).attr('data-iid', new_item_id);
+								$(itemReplaceLink).attr('href', pdpURLreplace);	
+
+								// replace the new item back into the array to keep it up to date
+								articleKeysArray0.splice(removeItemArtKey, 0, new_item_id);
+								console.log(articleKeysArray0);
+
+							},
+						});																		
+					}	
+					else if ($(this).attr('data-ens') == 1) {
+						articleKeysArray1.splice(removeItemArtKey, 1);
+						console.log("articleKeysArray after removing specific index: ");
+						console.log(articleKeysArray1);	
+						var url = "http://shopvcf.com/asiapi/shoptelligence_api_prod.php?items="+articleKeysArray1;
+						console.log(url);	
+
+						$.ajax({
+							method:"GET",
+							dataType: "json",
+							url: url,
+							success: function(data) {
+								console.log(data);
+
+								var replaceImgURL = data[1].ensemble[ensindex].image_url+"?impolicy=product-320x320";
+								//console.log(replaceImgURL);
+								var cKeyOrder = data[1].ensemble[ensindex].canvas_key_order;
+								var swatchImgURL = data[1].ensemble[ensindex].swatch_image_url;
+								var new_item_id = data[1].ensemble[ensindex].item_id;
+								//console.log(item_id);
+								var product_id = data[1].ensemble[ensindex].product_id;
+								//console.log(product_id);
+								var pdpURLreplace = 'http://www.valuecityfurniture.com/product/item/'+product_id;
+								console.log(pdpURLreplace);	
+								// replace the placeholder image, with replacement item
+								$(itemReplace).attr('src', replaceImgURL);	
+								// replace link
+								console.log(itemReplaceLink);
+								$(itemRepalceButton).attr('data-iid', new_item_id);
+								$(itemReplaceLink).attr('href', pdpURLreplace);	
+
+								// replace the new item back into the array to keep it up to date
+								articleKeysArray1.splice(removeItemArtKey, 0, new_item_id);
+								console.log(articleKeysArray1);
+
+							},
+						});																			
+					}
+					else if ($(this).attr('data-ens') == 2) {
+						articleKeysArray2.splice(removeItemArtKey, 1);
+						console.log("articleKeysArray after removing specific index: ");
+						console.log(articleKeysArray2);	
+						var url = "http://shopvcf.com/asiapi/shoptelligence_api_prod.php?items="+articleKeysArray2;
+						console.log(url);		
+
+						$.ajax({
+							method:"GET",
+							dataType: "json",
+							url: url,
+							success: function(data) {
+								console.log(data);
+
+								var replaceImgURL = data[1].ensemble[ensindex].image_url+"?impolicy=product-320x320";
+								//console.log(replaceImgURL);
+								var cKeyOrder = data[1].ensemble[ensindex].canvas_key_order;
+								var swatchImgURL = data[1].ensemble[ensindex].swatch_image_url;
+								var new_item_id = data[1].ensemble[ensindex].item_id;
+								//console.log(item_id);
+								var product_id = data[1].ensemble[ensindex].product_id;
+								//console.log(product_id);
+								var pdpURLreplace = 'http://www.valuecityfurniture.com/product/item/'+product_id;
+								console.log(pdpURLreplace);	
+								// replace the placeholder image, with replacement item
+								$(itemReplace).attr('src', replaceImgURL);	
+								// replace link
+								console.log(itemReplaceLink);
+								$(itemRepalceButton).attr('data-iid', new_item_id);
+								$(itemReplaceLink).attr('href', pdpURLreplace);	
+
+								// replace the new item back into the array to keep it up to date
+								articleKeysArray2.splice(removeItemArtKey, 0, new_item_id);
+								console.log(articleKeysArray2);
+
+							},
+						});																		
+					}		
+					else if ($(this).attr('data-ens') == 3) {
+						articleKeysArray3.splice(removeItemArtKey, 1);
+						console.log("articleKeysArray after removing specific index: ");
+						console.log(articleKeysArray3);				
+						var url = "http://shopvcf.com/asiapi/shoptelligence_api_prod.php?items="+articleKeysArray3;
+						console.log(url);	
+
+						$.ajax({
+							method:"GET",
+							dataType: "json",
+							url: url,
+							success: function(data) {
+								console.log(data);
+
+								var replaceImgURL = data[1].ensemble[ensindex].image_url+"?impolicy=product-320x320";
+								//console.log(replaceImgURL);
+								var cKeyOrder = data[1].ensemble[ensindex].canvas_key_order;
+								var swatchImgURL = data[1].ensemble[ensindex].swatch_image_url;
+								var new_item_id = data[1].ensemble[ensindex].item_id;
+								//console.log(item_id);
+								var product_id = data[1].ensemble[ensindex].product_id;
+								//console.log(product_id);
+								var pdpURLreplace = 'http://www.valuecityfurniture.com/product/item/'+product_id;
+								console.log(pdpURLreplace);	
+								// replace the placeholder image, with replacement item
+								$(itemReplace).attr('src', replaceImgURL);	
+								// replace link
+								console.log(itemReplaceLink);
+								$(itemRepalceButton).attr('data-iid', new_item_id);
+								$(itemReplaceLink).attr('href', pdpURLreplace);	
+
+								// replace the new item back into the array to keep it up to date
+								articleKeysArray3.splice(removeItemArtKey, 0, new_item_id);
+								console.log(articleKeysArray3);
+
+							},
+						});																
+					}					
+				});	
+
+				// Click handler for add to cart call
+				$("div[id^=key]").on('click','#addCartSingleProduct',function(){
+					// get the item_id of the single product that you want to 
+					// add to the cart
+					var singleItemCartAdd = $(this).data("cart");
+					$(this).addClass("addSingleProduct");
+					$(this).children(0).removeClass("default");
+
+					$("#controls").append("<div id='alert' class='alert alert-success alert-dismissible pull-right' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Item added to cart</strong></div>");
+
+					var stringSingleCartAdd = singleItemCartAdd.toString(); 
+					var data = {"itemid": stringSingleCartAdd, "quantity": "1"};
+
+					$.ajax({
+					     url: document.location.protocol+"//www.valuecityfurniture.com/api/shoppingcart/add",
+					     type: "GET",
+					     data: data,
+					    /* beforeSend: function(xhr){
+					     	xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+					     },*/
+					     success: function(data) { 
+					     },
+					     error: function(data) {
+					     	console.log('Error');
+					     },
+						 complete: function(data) {
+                         },					     
+					});				
+
+					// close the alert after some time
+					setTimeout(function(){
+  						$("div#alert").alert('close');
+					}, 1500);											
+				});		
+
+			}			
+		}
+
+		
+
+		var currentBoard = $('div[id^=ens]:visible').attr('id').substr(-1);
+		$("span.asi-cart-number").html(currentBoard);
+
+		// logic to step through ensembles
+		$("#next").click(function(){			
+			var currentBoard = $('div[id^=ens]:visible').attr('id').substr(-1);
+			console.log(currentBoard);
+			var currentBoardSelector = "div#ens"+currentBoard;
+			console.log("currentBoardSelector: "+currentBoardSelector);
+
+			var nextCount = parseInt(currentBoard) + 1;
+			console.log(nextCount);
+			$(".mixnum").append(currentBoard);
+
+			var nextBoardSelector = "div#ens"+nextCount;
+			console.log("nextBoardSelector: "+nextBoardSelector);
+
+			if (nextCount > 4) {			
+				$(currentBoardSelector).hide();		
+				$('div#ens1').show();			
+			}
+			// more boards to go, step to next
+			else {		
+				console.log("Else Clause, nextCount < 4")
+				$(currentBoardSelector).hide();	
+				$(nextBoardSelector).show();										
+			}
+		});
