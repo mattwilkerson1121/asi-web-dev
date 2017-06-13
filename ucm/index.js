5431305bb61d181db230d2193ebcceb9bffc22d5
joshua <jwells@CO01-MAC-JWELLS.local>
2017-06-12 22:38:16 -0400

    further refactoring
    unnecessary duplicated code removed.  reorganized so that I can find
    things that relate easier.

--- ucm/index.js
+++ ucm/index.js
@@ -1,17 +1,24 @@
-var postData;
-var email;
-var type;
-var ordertype;
-var response;
-var orderdate;
-var phone;
-var system;
-var requestid;
-var requesttype;
-var requestDate;
-var otid;
-var addr;
-var orderDate;
+// set extracted query params to a variable
+var email = getUrlParameter('email');
+var type = getUrlParameter('type');
+var ordertype = getUrlParameter('ordertype');
+var response = getUrlParameter('response').toLowerCase();
+var orderDate = getUrlParameter('orderDate');
+var phone = getUrlParameter('phone');
+var system = getUrlParameter('system');
+var requestid = getUrlParameter('requestid');
+var requesttype = getUrlParameter('requesttype');
+var otid = getUrlParameter('otid');
+var requestdate = getUrlParameter('requestdate');
+var addr = getUrlParameter('addr');
+
+function getUrlParameter(name) {
+	// return value of query parameter
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
 function defaultCustSrvcNum() {
 	if (document.location.host === "www.valuecityfurniture.com") {
@@ -22,14 +29,6 @@ function defaultCustSrvcNum() {
 		var defaultCustSrvcNum = "1-888-751-8552";
 		return defaultCustSrvcNum;
 	}
-}
-
-function getUrlParameter(name) {
-	// return value of query parameter
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
 function formatDate(date) {
@@ -90,7 +89,6 @@ function testQueryParams() {
 	else if ((requesttype == "WINDOW") && (type == "r") && (response == "r") && (system != "") && (requestid != "") && (otid != "") && (ordertype != "")) {
 		return "reschedule-window"			
 	}
-
 	else {
 		return "default"		
 	}
@@ -113,49 +111,36 @@ function responseAPI() {
   		error: function(data) {
   			console.log(data);
   		}
-    	
 	});		
 }
 
-// set extracted query params to a variable
-var email = getUrlParameter('email');
-var type = getUrlParameter('type');
-var ordertype = getUrlParameter('ordertype');
-var response = getUrlParameter('response').toLowerCase();
-var orderDate = getUrlParameter('orderDate');
-var phone = getUrlParameter('phone');
-var system = getUrlParameter('system');
-var requestid = getUrlParameter('requestid');
-var requesttype = getUrlParameter('requesttype');
-var otid = getUrlParameter('otid');
-var requestdate = getUrlParameter('requestdate');
-var addr = getUrlParameter('addr');
+
 // store the message type results to control UI and also responseAPI() call
 var result = testQueryParams();
 
 if (result == "confirm-pcc") {
 	console.log("confirm-pcc");
 	$("#custom-page-body .asi-container").append("<div class='asi-ucm' style='margin-top: 0%'><p>Thanks for confirming your "+ordertype+".<br /><br/>We'll see you on <span style='color:#e10e32;font-weight:bold'>"+formatDate(orderDate)+"</span>.<br/><br/>For information about your order try our <a href='/track-my-order?tracking="+otid+"' style='color:#e10e32;font-weight:bold;'>order tracker</a>.<br /></p><div class='asi-red-separator' style='margin-top: 10px;margin-bottom:20px;'></div></div>");	
-
+	// Send back email response through API 
 	responseAPI();		
 }
 else if (result == "reschedule-pcc") {
 	console.log("reschedule-pcc");	
 	$("#custom-page-body .asi-container").append("<div class='asi-ucm' style='margin-top:0%;'><p>Your request has been received. You may reach the store at:<br/><br/><span style='color:#e10e32;font-weight:bold;'><a href='tel:1"+phone+"' target='_blank' style='color:#e10e32;font-weight:bold'>"+phone+"</a></span><br /><br />Otherwise, you will be contacted as soon as possible.<br /><br/><br/>For information about your order try our <a href='/track-my-order?tracking="+otid+"' style='color:#e10e32;font-weight:bold;'>order tracker</a>.<br/><br />Thank you!</p><div class='asi-red-separator' style='margin-top:10px;margin-bottom:20px;'></div></div>");	
-
+	// Send back email response through API 
 	responseAPI();		
 }
 else if (result == "confirm-window") {
 	console.log("confirm-window");
 
 	$("#custom-page-body .asi-container").append("<div class='asi-ucm' style='margin-top: 0%'><p>Yay! Thanks for confirming your "+ordertype+".<br /><br/>We'll see you "+daysTillOrderDate(orderDate)+" <span style='color:#e10e32;font-weight:bold'>"+formatDate(orderDate)+"</span> at <span style='color:#e10e32;font-weight:bold'>"+addr+"</span>.<br/><br/>If you have any questions between now and then, feel free to give us a call at:<br/><br/><a href='tel:1"+phone+"' target='_blank' style='color:#e10e32;font-weight:bold'>"+phone+"</a><br/><br/>For information about your order try our <a href='/track-my-order?tracking="+otid+"' style='color:#e10e32;font-weight:bold;'>order tracker</a>.<br /></p><div class='asi-red-separator' style='margin-top: 10px;margin-bottom:20px;'></div></div>");	
-
+	// Send back email response through API 
 	responseAPI();			
 }
 else if (result == "reschedule-window") {
 	console.log("reschedule-window");	
 	$("#custom-page-body .asi-container").append("<div class='asi-ucm' style='margin-top:0%;'><p>Your request has been received. You may reach the store at:<br/><br/><span style='color:#e10e32;font-weight:bold;'><a href='tel:1"+phone+"' target='_blank' style='color:#e10e32;font-weight:bold'>"+phone+"</a></span><br /><br />Otherwise, you will be contacted as soon as possible.<br /><br/><br/>For information about your order try our <a href='/track-my-order?tracking="+otid+"' style='color:#e10e32;font-weight:bold;'>order tracker</a>.<br/><br />Thank you!</p><div class='asi-red-separator' style='margin-top:10px;margin-bottom:20px;'></div></div>");	
-
+	// Send back email response through API 
 	responseAPI();			
 }
 else {
