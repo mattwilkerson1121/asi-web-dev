7fc6df3d0cb2412a39305ac93c95502a0506276d
nwest95 <nwest95@gmail.com>
2017-09-08 10:54:42 -0400

    Add files via upload

---
+++ mamp/htdocs/asi-home-4-spot-promo.php
@@ -0,0 +1,190 @@
+<?php include_once('includes/asiSiteHeader.php'); ?>
+<?php include_once('includes/asiSiteTopNavigation.php'); ?>
+
+ 
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-416">
+            <div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="asi-offer-background-image">
+                            <a href="#"><img src="/images/D_Sale_Accents_Img_09Wk3Y17.png" class="img-responsive"></a>
+                        </div>
+                           <div class="asi-offer-content" style="position:relative;z-index:2;margin-top:-10px;">
+                                <div class="asi-offer-content-box" style="margin-top:0 !important;padding-top:0 !important;">
+                                    <div style="position:absloute;background:#ec3a42;width:100%;height:8px;padding:0;margin:0;top:-8px;left:0;"></div>
+                                    <h3 class="asi-content-box">10% SELECT DINING ROOMS</h3>
+                                        <h4 class="asi-content-box">SUB-HEADER TEXT</h4>
+                                            <p><a href="#" data-pos="1">SHOP NOW &gt;</a></p>
+                                </div>
+                            </div>
+                    </div>   
+
+
+                           <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="asi-offer-background-image">
+                            <a href="#"><img src="/images/D_Sale_Accents_Img_09Wk3Y17.png" class="img-responsive"></a>
+                        </div>
+                           <div class="asi-offer-content" style="position:relative;z-index:2;margin-top:-10px;">
+                                <div class="asi-offer-content-box" style="margin-top:0 !important;padding-top:0 !important;">
+                                    <div style="position:absloute;background:#ec3a42;width:100%;height:8px;padding:0;margin:0;top:-8px;left:0;"></div>
+                                    <h3 class="asi-content-box">10% SELECT DINING ROOMS</h3>
+                                        <h4 class="asi-content-box">SUB-HEADER TEXT</h4>
+                                            <p><a href="#" data-pos="1">SHOP NOW &gt;</a></p>
+                                </div>
+                            </div>
+
+
+
+                    </div> 
+
+
+
+     </div>
+
+
+            </div>
+        </div>
+    </div>
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-417">
+            <div>  <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-416">
+            <div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="asi-offer-background-image">
+                            <a href="#"><img src="/images/D_Sale_Accents_Img_09Wk3Y17.png" class="img-responsive"></a>
+                        </div>
+                           <div class="asi-offer-content" style="position:relative;z-index:2;margin-top:-10px;">
+                                <div class="asi-offer-content-box" style="margin-top:0 !important;padding-top:0 !important;">
+                                    <div style="position:absloute;background:#ec3a42;width:100%;height:8px;padding:0;margin:0;top:-8px;left:0;"></div>
+                                    <h3 class="asi-content-box">10% SELECT DINING ROOMS</h3>
+                                        <h4 class="asi-content-box">SUB-HEADER TEXT</h4>
+                                            <p><a href="#" data-pos="1">SHOP NOW &gt;</a></p>
+                                </div>
+                            </div>
+                    </div>   
+
+
+                           <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="asi-offer-background-image">
+                            <a href="#"><img src="/images/D_Sale_Accents_Img_09Wk3Y17.png" class="img-responsive"></a>
+                        </div>
+                           <div class="asi-offer-content" style="position:relative;z-index:2;margin-top:-10px;">
+                                <div class="asi-offer-content-box" style="margin-top:0 !important;padding-top:0 !important;">
+                                    <div style="position:absloute;background:#ec3a42;width:100%;height:8px;padding:0;margin:0;top:-8px;left:0;"></div>
+                                    <h3 class="asi-content-box">10% SELECT DINING ROOMS</h3>
+                                        <h4 class="asi-content-box">SUB-HEADER TEXT</h4>
+                                            <p><a href="#" data-pos="1">SHOP NOW &gt;</a></p>
+                                </div>
+                            </div>
+
+
+
+                    </div> 
+
+
+
+     </div>
+
+
+            </div>
+        </div>
+    </div></div>
+        </div>
+    </div>
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-418">
+            <div></div>
+        </div>
+    </div>
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-419">
+            <div></div>
+        </div>
+    </div>
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-420">
+            <div></div>
+        </div>
+    </div>
+
+
+    <!-- Home-row-15 -->
+    <!-- ASI CSS + JS SCRIPTS !-->
+    <div class="row-fluid home-content-row clearfix">
+        <div class="pe-type-id-431">
+	        <div>
+                <!-- ASI CSS OVERRIDES - DO NOT ALTER !--> 
+				<link rel="stylesheet" type="text/css" href="css/asi-mix-styles.css" />
+				<link rel="stylesheet" type="text/css" href="css/asi-mix-styles-main.css" />
+                <!-- ASI JS SCRIPTS - DO NOT ALTER !-->
+                <script type="text/javascript" src="js/asi-mix-scripts.js"></script>
+			</div>
+		</div>
+    </div>
+<style>
+.asi-offer-content-box{
+    background-color:#ec3a42;
+     margin-left:30px;
+    margin-right:30px;
+    padding-top:20px;
+    padding-bottom:20px;
+}
+.asi-offer-content-box p{
+    text-align:center;
+    padding-top:20px;
+}
+.asi-offer-content-box a{
+    color:#fff;
+    font-family: 'ProximaNova-Bold';
+    font-size: 16px;
+    color: #FFFFFF;
+    letter-spacing: 1.85px;
+    line-height: 18px;
+    padding-top:20px;
+
+}
+
+.asi-offer-content h3{
+    text-align:center;
+    font-family: 'Knockout-49Liteweight', sans-serif;
+    font-size: 42px;
+    color: #FFFFFF;
+    letter-spacing: 1.75px;
+    line-height: 44px;
+}
+.asi-offer-content h4{
+    text-align:center;
+    font-family: 'ProximaNova-Bold', sans-serif;
+    font-size: 18px;
+    color: #FFFFFF;
+    letter-spacing: 1.8px;
+    line-height: 22px;
+}
+@media (max-width: 768px) {
+
+.asi-offer-content h3{
+    text-align:center;
+    font-family: 'Knockout-49Liteweight', sans-serif;
+    font-size: 28px;
+    color: #FFFFFF;
+    letter-spacing: 1.75px;
+    line-height: 44px;
+}
+.asi-offer-content h4{
+    text-align:center;
+    font-family: 'ProximaNova-Bold', sans-serif;
+    font-size: 18px;
+    color: #FFFFFF;
+    letter-spacing: 1.8px;
+    line-height: 22px;
+}
+</style>
+
+
+
+
+<?php include_once('includes/asiSitePreFooter.php'); ?>
+<?php include_once('includes/asiSiteFooter.php'); ?>
